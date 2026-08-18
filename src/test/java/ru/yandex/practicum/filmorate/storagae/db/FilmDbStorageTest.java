8935a40180cce0288d47715c562a1e5c1776a007
Artem <malibu2903@yandex.ru>
2026-08-18 14:31:11 +0300

    Исправил тест

--- src/test/java/ru/yandex/practicum/filmorate/storagae/db/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storagae/db/FilmDbStorageTest.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ContextConfiguration;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.MPARating;
 import ru.yandex.practicum.filmorate.storage.db.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.db.mapper.FilmRowMapper;
+import ru.yandex.practicum.filmorate.storage.db.mapper.GenreRowMapper;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.time.LocalDate;
@@ -17,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @JdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ContextConfiguration(classes = {FilmDbStorage.class, FilmRowMapper.class})
+@ContextConfiguration(classes = {FilmDbStorage.class, FilmRowMapper.class, GenreRowMapper.class})
 class FilmDbStorageTest {
 
     @Autowired
@@ -30,14 +32,14 @@ class FilmDbStorageTest {
                 .description("A mind-bending thriller")
                 .releaseDate(LocalDate.of(2010, 7, 16))
                 .duration(148)
-                .mpaId(3)
+                .mpa(MPARating.builder().id(3).build())
                 .build();
 
         Film created = filmStorage.create(film);
 
         assertThat(created.getId()).isPositive();
         assertThat(created.getName()).isEqualTo("Inception");
-        assertThat(created.getMpaId()).isEqualTo(3);
+        assertThat(created.getMpa().getId()).isEqualTo(3);
     }
 
     @Test
@@ -48,7 +50,7 @@ class FilmDbStorageTest {
                 .description("Old desc")
                 .releaseDate(LocalDate.of(2020, 1, 1))
                 .duration(100)
-                .mpaId(1)
+                .mpa(MPARating.builder().id(1).build())
                 .build();
 
         Film created = filmStorage.create(film);
@@ -56,13 +58,13 @@ class FilmDbStorageTest {
         // Обновляем
         created.setName("Updated Name");
         created.setDescription("Updated description");
-        created.setMpaId(2);
+        created.setMpa(MPARating.builder().id(2).build());
 
         Film updated = filmStorage.update(created);
 
         assertThat(updated.getName()).isEqualTo("Updated Name");
         assertThat(updated.getDescription()).isEqualTo("Updated description");
-        assertThat(updated.getMpaId()).isEqualTo(2);
+        assertThat(updated.getMpa().getId()).isEqualTo(2);
     }
 
     @Test
@@ -72,7 +74,7 @@ class FilmDbStorageTest {
                 .description("A fish adventure")
                 .releaseDate(LocalDate.of(2003, 5, 30))
                 .duration(100)
-                .mpaId(2)
+                .mpa(MPARating.builder().id(2).build())
                 .build();
 
         Film created = filmStorage.create(film);
@@ -81,6 +83,6 @@ class FilmDbStorageTest {
 
         assertThat(found).isPresent();
         assertThat(found.get().getName()).isEqualTo("Finding Nemo");
-        assertThat(found.get().getMpaId()).isEqualTo(2);
+        assertThat(found.get().getMpa().getId()).isEqualTo(2);
     }
 }
